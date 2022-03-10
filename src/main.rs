c723f1cd8a6a2d529695f9020bdb4b41523a51e3
Sandra ter Maat <HS.terMaat@student.han.nl>
2022-03-10 16:17:34 +0100

    Set a specified percentage of numbers to blank (0) and print the solvable soduko and its solution

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,31 @@ use std::{self, vec};
 
 //Rust doesn't have a sqrt function for integers / usize
 const NR_OF_COL_AS_FLOAT: f32 = 9.0;
+const PERCENTAGE_HIDDEN: f32 = 70.0;
+
 const NR_OF_COL: usize = NR_OF_COL_AS_FLOAT as usize;
 
 fn main() {
-    generate_filled_soduko();
+    let solution = generate_solution();
+    generate_sudoku(&solution, PERCENTAGE_HIDDEN);
 }
 
-fn generate_filled_soduko(){
+fn generate_sudoku(solution: &Vec<u8>, percentage_hidden: f32) {
+    let mut sudoku = solution.clone();
+    let numbers_hidden = (NR_OF_COL_AS_FLOAT.powf(2.0) / 100.0 * percentage_hidden) as usize;
+
+    for _i in 0..numbers_hidden {
+        let hidded_nr_index: usize =
+            generate_number(0, (NR_OF_COL_AS_FLOAT.powf(2.0) - 1.0) as u8) as usize;
+        sudoku[hidded_nr_index] = 0;
+    }
+
+    print_matrix(&sudoku);
+    println!("\n Solution");
+    print_matrix(&solution);
+}
+
+fn generate_solution() -> Vec<u8> {
     let mut nr_of_tries: u128 = 1;
     let mut succes = true;
     let mut matrix = vec![0; NR_OF_COL.pow(2)];
@@ -27,8 +45,8 @@ fn generate_filled_soduko(){
             }
         }
     }
-    println!("    Generated in {} tries", nr_of_tries);
-    print_matrix(&matrix);
+    println!("\n \n Sudoko was generated in {} tries", nr_of_tries);
+    matrix
 }
 
 fn fill_mini_matrix(
@@ -60,8 +78,6 @@ fn fill_mini_matrix(
 
             let mut valid: bool = false;
             while !valid {
-                let number = generate_number();
-
                 if used_numbers_matrix.len() != NR_OF_COL {
                     if no_available_numbers(
                         &used_numbers_row,
@@ -71,6 +87,7 @@ fn fill_mini_matrix(
                         return Err(false);
                     }
 
+                    let number = generate_number(1, (NR_OF_COL + 1) as u8);
                     if check_valid(&used_numbers_matrix, number)
                         && check_valid(&used_numbers_row, number)
                         && check_valid(&used_numbers_col, number)
@@ -86,9 +103,9 @@ fn fill_mini_matrix(
     Ok(matrix)
 }
 
-fn generate_number() -> u8 {
+fn generate_number(low: u8, high: u8) -> u8 {
     let mut rng = rand::thread_rng();
-    let number = rng.gen_range(1, (NR_OF_COL + 1) as u8);
+    let number = rng.gen_range(low, high);
     number
 }
 
@@ -126,7 +143,7 @@ fn wipe_mini_matrix(start_row: usize, start_col: usize, matrix: &mut Vec<u8>) ->
 
 fn fill_all(matrix: &mut Vec<u8>) -> Result<&mut Vec<u8>, bool> {
     for matrices_in_row in 0..NR_OF_COL_AS_FLOAT.sqrt() as usize {
-        for matrices_in_col in 0..NR_OF_COL_AS_FLOAT.sqrt() as usize{
+        for matrices_in_col in 0..NR_OF_COL_AS_FLOAT.sqrt() as usize {
             fill_mini_matrix(
                 matrices_in_row * NR_OF_COL_AS_FLOAT.sqrt() as usize,
                 matrices_in_col * NR_OF_COL_AS_FLOAT.sqrt() as usize,
@@ -165,5 +182,5 @@ fn print_matrix(matrix: &Vec<u8>) {
         }
         println!(" ");
     }
-    println!("\n ============ end of matrix =============== \n")
+    println!("\n ========================================= \n")
 }
